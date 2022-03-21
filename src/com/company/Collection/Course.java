33236bf5ba3ea4a58e3ee57341ab230ae54e23d6
Venkatesh Prasad V <venkateshprasad0@gmail.com>
2022-03-21 14:54:30 +0530

    Collection Comparablw

--- src/com/company/Collection/Course.java
+++ src/com/company/Collection/Course.java
@@ -2,6 +2,8 @@ package com.company.Collection;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Course implements Comparable<Course> {
     public int courseId;
@@ -32,6 +34,16 @@ public class Course implements Comparable<Course> {
         System.out.println(courseList);
 
 // output: [124:AngularJS, 121:Hibernate, 120:Java]
+
+
+        Set<Course> courseSet = new TreeSet<>();
+        courseSet.add(new Course(124, "AngularJS"));
+        courseSet.add(new Course(120, "Java"));
+        courseSet.add(new Course(121, "Hibernate"));
+        System.out.println(courseSet);
+
+// output: [124:AngularJS, 121:Hibernate, 120:Java]
+
 
     }
 }
